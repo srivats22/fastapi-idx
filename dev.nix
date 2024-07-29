1d2a239d4d0c92d20e1c771eeb92179431a575aa
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2024-07-29 17:09:57 -0400

    Update dev.nix

--- dev.nix
+++ dev.nix
@@ -17,13 +17,15 @@
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         install =
-          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
+          "pip install -r requirements.txt";
         # Open editors for the following files by default, if they exist:
         default.openFiles = [ "app.py" ];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
     };
     # Enable previews and customize configuration
-    previews = {};
+    previews = {
+      run-preview = "uvicorn app:app --reload"
+    };
   };
 }
