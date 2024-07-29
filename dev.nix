d80a5e38dfe5bad3ea94ac43d54bac576eb5d34d
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2024-07-29 17:12:34 -0400

    Update dev.nix

--- dev.nix
+++ dev.nix
@@ -24,8 +24,6 @@
       # To run something each time the workspace is (re)started, use the `onStart` hook
     };
     # Enable previews and customize configuration
-    previews = {
-      run-preview = "uvicorn app:app --reload"
-    };
+    previews = {};
   };
 }
